b8760f2ff40490b33732c6a8a23fe6488f9caa35
Chychen <jaycase.cs01@gmail.com>
2017-04-30 16:34:54 +0800

    refactoring ...

--- ui_labeling/ui_labeling.py
+++ ui_labeling/ui_labeling.py
@@ -23,6 +23,7 @@ import kivy
 kivy.require('1.8.0')
 Config.set('graphics', 'width', '1800')
 Config.set('graphics', 'height', '1000')
+# use both left click and right click from mouse in this app
 Config.set('input', 'mouse', 'mouse, disable_multitouch')
 from preprocessing.sphere_fitting import fit_sphere
 
@@ -35,14 +36,23 @@ LABELED_DATA_DIR_PATH = os.path.join(DIR_PATH, 'labeled_voc')
 
 
 class UserIDTextInput(BoxLayout):
+    """
+    A dialog with TextInput, users could enter their Uid, and comfirm with 'Enter' button.
+    """
     button_text = StringProperty("")
 
     def __init__(self, *args, **kwargs):
+        """
+        params: kwargs['on_enter']: The callback function for user hit button 'Enter', which is bound to the TextInput.on_text_validate in .kv file.
+        """
         super(UserIDTextInput, self).__init__(*args, **kwargs)
         self.on_enter = kwargs['on_enter']
 
 
 class ContentWithButton(BoxLayout):
+    """
+    A dialog with Button
+    """
     content_text = StringProperty("")
     button_text = StringProperty("")
 
@@ -52,22 +62,27 @@ class ContentWithButton(BoxLayout):
         self.button_text = kwargs['button_text']
 
     def exit(self):
+        """
+        The callback function to exit the program, which is bound to the Button.on_release in .kv file.
+        """
         App.get_running_app().stop()
 
 
-class StartCursor(Widget):
-    def __init__(self, *args, **kwargs):
-        super(StartCursor, self).__init__(*args, **kwargs)
-        self.rgb = kwargs['color']
+class Cursor(Widget):
+    """
+    The customed widget to draw cursor on the SlideBar.
+    """
 
-
-class EndCursor(Widget):
     def __init__(self, *args, **kwargs):
-        super(EndCursor, self).__init__(*args, **kwargs)
+        super(Cursor, self).__init__(*args, **kwargs)
         self.rgb = kwargs['color']
 
 
 class SlideBar(Widget):
+    """
+    The customed widget to draw SlideBar on the canvas.
+    """
+    # the height of the SlideBar
     y_offset = NumericProperty(30.0)
 
     def __init__(self, *args, **kwargs):
@@ -75,56 +90,74 @@ class SlideBar(Widget):
 
 
 class DrawingBoard(Widget):
+    """
+    The customed widget to draw trajectories (points and lines) on the canvas.
+    """
 
     def __init__(self, *args, **kwargs):
         super(DrawingBoard, self).__init__(*args, **kwargs)
-        self.y_offset = 200.0
+        # board's state
+        self.isInit = False
+        self.voc_length = None
+
+        # points (timesteps in trajectory)
         self.points = []
         self.all_selected_points_list = []
+        self.all_selected_line_list = []
+        # communicating labeled infos between canvas board and io data
         self.all_selected_points_idx_list = []
+
+        # cursors
         self.all_cursor_list = []
-        self.all_canvas_selected_line_list = []
-        self.all_connectionist_color_list = []
         self.all_cursor_lines_list = []
-        self.voc_length = None
-        self.isInit = False
+
+        # colors
+        self.all_connectionist_color_list = []
         self.high_contrast_colors_index = 0
         self.high_contrast_colors = [Color(1, .3, .3),
                                      Color(.3, 1, .3),
                                      Color(.3, .3, 1),
                                      Color(1, .1, 1),
                                      Color(1, 1, .3)]
+
+        # variables to update cursor with user's mouse input
         self.closest_cursor = None
         self.closest_cursor_id = None
 
     def init_board(self, points, voc_length, restored_labeled_list=None):
+        """
+        params: points: list of trajectories' posistions, read from file
+        params: voc_length: integer, length of target vocabulary
+        params: restored_labeled_list: list of boolean value, recording the index of labeled positions. if None, init by default.
+        """
         self.isInit = True
-        self.canvas.clear()
         self.points = points
         self.voc_length = voc_length
-        voc_lines = Line(points=self.points, width=4)
-        self.canvas.add(Color(.6, .6, .6, .6))
-        self.canvas.add(voc_lines)
+
+        # re-draw basic trajectory on canvas
+        self.canvas.clear()
         voc_points = Point(points=self.points, pointsize=5)
         self.canvas.add(Color(.6, .6, .6, 1))
         self.canvas.add(voc_points)
 
+        voc_lines = Line(points=self.points, width=4)
+        self.canvas.add(Color(.6, .6, .6, .6))
+        self.canvas.add(voc_lines)
+
+        # init cursors and selected points on canvas
         if restored_labeled_list is not None:
             self.init_restored(restored_labeled_list)
         else:
             self.init_default()
 
+        # update all selected points index by cursors' positions
         self.update_selected_points()
 
-    def get_color(self):
-        return_color = self.high_contrast_colors[self.high_contrast_colors_index]
-        self.high_contrast_colors_index += 1
-        if self.high_contrast_colors_index == len(self.high_contrast_colors):
-            self.high_contrast_colors_index = 0
-        return return_color
-
     def init_restored(self, restored_labeled_list):
-        # restore cursors and correspond selected points
+        """
+        restore cursors and selected points on canvas
+        params: restored_labeled_list: list of boolean value, recording the index of labeled positions.
+        """
         self.all_connectionist_color_list = []
         for i in range(self.voc_length):
             self.all_connectionist_color_list.append(self.get_color())
@@ -153,13 +186,13 @@ class DrawingBoard(Widget):
                     # start cursor
                     start_x = (float(temp_start_idx) /
                                len(restored_labeled_list))
-                    temp_start_cursor = StartCursor(
+                    temp_start_cursor = Cursor(
                         pos=(start_x * self.width - 5, SlideBar().y_offset), color=self.all_connectionist_color_list[counter].rgb)
                     self.add_widget(temp_start_cursor)
                     self.all_cursor_list.append(temp_start_cursor)
                     # end cursor
                     end_x = (float(temp_end_idx) / len(restored_labeled_list))
-                    temp_end_cursor = EndCursor(
+                    temp_end_cursor = Cursor(
                         pos=(end_x * self.width - 5, SlideBar().y_offset), color=self.all_connectionist_color_list[counter].rgb)
                     self.add_widget(temp_end_cursor)
                     self.all_cursor_list.append(temp_end_cursor)
@@ -174,36 +207,32 @@ class DrawingBoard(Widget):
                     # color index
                     counter += 1
 
-        # visulize selected points on canvas and record pointers of those
-        # canvas's Lines
-        self.all_canvas_selected_line_list = []
-        for i, selected_points in enumerate(self.all_selected_points_list):
-            # temp_P = Point(points=selected_points, pointsize=5)
-            temp_P = Line(points=selected_points, width=3)
-            self.all_canvas_selected_line_list.append(temp_P)
-            self.canvas.add(self.all_connectionist_color_list[i])  # add Color
-            self.canvas.add(temp_P)  # add Line
+        # visulize Lines between selected points on canvas
+        self.add_lines_between_selected_points()
 
     def init_default(self):
-        # add default cursors and correspond selected points
+        """
+        init default cursors and selected points on canvas
+        """
         self.all_selected_points_list = []
         self.all_cursor_list = []
         self.all_connectionist_color_list = []
         self.all_cursor_lines_list = []
         # cursor_range = 1 / (2 * voc_length - 1)
-        # cursor-i: range(2 * i * cursor_range, (2 * i + 1) * cursor_range)
+        # cursor-i: range((2 * i + 0) * cursor_range, (2 * i + 1) *
+        # cursor_range)
         cursor_range = 1.0 / (2 * self.voc_length - 1)
         for i in range(self.voc_length):
             self.all_connectionist_color_list.append(self.get_color())
             # start cursor
             start_x = 2 * i * cursor_range
-            temp_start_cursor = StartCursor(
+            temp_start_cursor = Cursor(
                 pos=(start_x * self.width - 5, SlideBar().y_offset), color=self.all_connectionist_color_list[i].rgb)
             self.add_widget(temp_start_cursor)
             self.all_cursor_list.append(temp_start_cursor)
             # end cursor
             end_x = (2 * i + 1) * cursor_range
-            temp_end_cursor = EndCursor(
+            temp_end_cursor = Cursor(
                 pos=(end_x * self.width - 5, SlideBar().y_offset), color=self.all_connectionist_color_list[i].rgb)
             self.add_widget(temp_end_cursor)
             self.all_cursor_list.append(temp_end_cursor)
@@ -221,15 +250,52 @@ class DrawingBoard(Widget):
                                                2: end_point_idx * 2]
             self.all_selected_points_list.append(temp_selected_points)
 
-        # record pointers pointing to canvas's Point in
-        # 'self.all_canvas_point_list'
-        self.all_canvas_selected_line_list = []
+        # visulize Lines between selected points on canvas
+        self.add_lines_between_selected_points()
+
+    def get_color(self):
+        """
+        return: return_color: Color, one of color in self.high_contrast_colors, get color in order.
+        """
+        return_color = self.high_contrast_colors[self.high_contrast_colors_index]
+        self.high_contrast_colors_index += 1
+        if self.high_contrast_colors_index == len(self.high_contrast_colors):
+            self.high_contrast_colors_index = 0
+        return return_color
+
+    def add_lines_between_selected_points(self):
+        self.all_selected_line_list = []
         for i, selected_points in enumerate(self.all_selected_points_list):
-            # temp_P = Point(points=selected_points, pointsize=5)
             temp_P = Line(points=selected_points, width=3)
-            self.all_canvas_selected_line_list.append(temp_P)
+            self.all_selected_line_list.append(temp_P)
             self.canvas.add(self.all_connectionist_color_list[i])
             self.canvas.add(temp_P)
+
+    def get_cursor_matched_point_idx(self, cursor):
+        """
+        match cursor.center_x (float) to index (integer) in trajectory list
+        params: cursor: Cursor
+        """
+        if cursor.center_x > self.width - 5:
+            cursor.center_x = self.width
+        normalized_x = cursor.center_x / self.width
+        pointsLength = len(self.points) / 2
+        return int(normalized_x * pointsLength)
+
+    def update_selected_points(self):
+        """
+        update all selected points index by cursors' positions
+        """
+        self.all_selected_points_idx_list = []  # clear
+        for i, canvas_selected_line in enumerate(self.all_selected_line_list):
+            startPtIdx = self.get_cursor_matched_point_idx(
+                self.all_cursor_list[i * 2])
+            endPtIdx = self.get_cursor_matched_point_idx(
+                self.all_cursor_list[i * 2 + 1])
+            canvas_selected_line.points = self.points[startPtIdx *
+                                                      2: endPtIdx * 2]
+            for selected_idx in range(startPtIdx, endPtIdx, 1):
+                self.all_selected_points_idx_list.append(selected_idx)
 
     def on_touch_move(self, touch):
         super(DrawingBoard, self).on_touch_down(touch)
@@ -241,28 +307,9 @@ class DrawingBoard(Widget):
         if self.isInit:
             self.touch_action(touch, mode='on_touch_down')
 
-    def get_cursor_matched_point_idx(self, cursor):
-        if cursor.center_x > self.width - 5:
-            cursor.center_x = self.width
-        normalized_x = cursor.center_x / self.width
-        pointsLength = len(self.points) / 2
-        return int(normalized_x * pointsLength)
-
-    def update_selected_points(self):
-        self.all_selected_points_idx_list = []  # clear
-        for i, canvas_selected_line in enumerate(self.all_canvas_selected_line_list):
-            startPtIdx = self.get_cursor_matched_point_idx(
-                self.all_cursor_list[i * 2])
-            endPtIdx = self.get_cursor_matched_point_idx(
-                self.all_cursor_list[i * 2 + 1])
-            canvas_selected_line.points = self.points[startPtIdx *
-                                                      2: endPtIdx * 2]
-            for selected_idx in range(startPtIdx, endPtIdx, 1):
-                self.all_selected_points_idx_list.append(selected_idx)
-
     def touch_action(self, touch, mode):
         if mode == 'on_touch_down':
-            # select the cloest cursor to modify its center_x
+            # select the cloest cursor and its index to modify its center_x 
             self.closest_cursor = None
             self.closest_cursor_id = -1
             if touch.x > self.all_cursor_list[-1].center_x and touch.x < self.width:
@@ -290,32 +337,33 @@ class DrawingBoard(Widget):
                             else:
                                 self.closest_cursor = self.all_cursor_list[i + 1]
                                 self.closest_cursor_id = i + 1
-
+        
+        # update cursor's position
         if self.closest_cursor is not None:
             if touch.x < self.width and touch.y < self.height:
                 self.closest_cursor.center_x = touch.x
 
-        # make sure the cursor's center_x value won't exceed neighbors
+        # make sure the cursor's center_x value won't exceed others
         # '+-5' is for foolproof
         if self.closest_cursor is not None:
             if self.closest_cursor_id > 0 and self.closest_cursor_id < len(self.all_cursor_list) - 1:
                 for any_i, any_cursor in enumerate(self.all_cursor_list):
                     if any_i > self.closest_cursor_id and any_cursor.center_x < self.closest_cursor.center_x + 5:
-                        any_cursor.center_x = self.closest_cursor.center_x + 5 * (any_i-self.closest_cursor_id)
+                        any_cursor.center_x = self.closest_cursor.center_x + \
+                            5 * (any_i - self.closest_cursor_id)
                     if any_i < self.closest_cursor_id and any_cursor.center_x > self.closest_cursor.center_x - 5:
-                        any_cursor.center_x = self.closest_cursor.center_x - 5 * (self.closest_cursor_id - any_i)
+                        any_cursor.center_x = self.closest_cursor.center_x - \
+                            5 * (self.closest_cursor_id - any_i)
 
         # update all lins bwtween cursors
         for any_i, _ in enumerate(self.all_cursor_list):
             if any_i % 2 == 0:
-                temp = self.all_cursor_lines_list[int(any_i/2)].points
+                temp = self.all_cursor_lines_list[int(any_i / 2)].points
                 temp[0] = self.all_cursor_list[any_i].center_x
                 temp[2] = self.all_cursor_list[any_i + 1].center_x
-                self.all_cursor_lines_list[int(any_i/2)].points = temp
-                
+                self.all_cursor_lines_list[int(any_i / 2)].points = temp
 
         self.update_selected_points()
-
 
 
 class AppEngine(FloatLayout):
@@ -331,15 +379,18 @@ class AppEngine(FloatLayout):
 
     def __init__(self, *args, **kwargs):
         super(AppEngine, self).__init__(*args, **kwargs)
-        self.vocs_amount = None
-        self.result_dict = None
+        # file infos
         self.user_id = None
-        self.vocs_idx_counter = None
+        self.vocs_amount = None
         self.normalized_dirpath = None
         self.result_dirpath = None
         self.words_list = None
+        self.vocs_idx_counter = None
+        
+        # the labeled word's data
+        self.result_dict = None
 
-        # create content and add to the popup
+        # popup dialog ask for user id
         self.create_userid_textinput(title="User ID")
 
     def create_userid_textinput(self, title):
@@ -351,14 +402,22 @@ class AppEngine(FloatLayout):
                                  content=content,
                                  auto_dismiss=False,
                                  size_hint=(.15, .25))
-        # open the popup
+        # open the popup dialog
         self.popupUserID.open()
 
     def on_enter(self, user_id):
+        """
+        callback function when button 'Enter' is pressed
+        params: user_id: integer, user's id, get from popupUserID popup dialog
+        """
         self.popupUserID.dismiss()
         self.init(user_id)
 
     def init(self, user_id):
+        """
+        read/write json files according to user_id
+        params: user_id: integer, user's id, get from popupUserID popup dialog
+        """
         self.lastButton.bind(on_release=self.lastButtonCallback)
         self.nextButton.bind(on_release=self.nextButtonCallback)
         self.user_id = user_id
@@ -401,13 +460,11 @@ class AppEngine(FloatLayout):
 
     def lastButtonCallback(self, instance):
         self.update_final_dict()
-        # move to last word
         print ('!!!! Move to <Last> Word !!!!')
         self.move_last_voc()
 
     def nextButtonCallback(self, instance):
         self.update_final_dict()
-        # move to next word
         print ('!!!! Move to <Next> Word !!!!')
         self.move_next_voc()
 
