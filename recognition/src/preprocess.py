8df7465aab7b3cd81d543f86799232ea5e994e76
Chychen <jaycase.cs01@gmail.com>
2017-07-01 19:51:24 +0800

    debugging []

--- recognition/src/preprocess.py
+++ recognition/src/preprocess.py
@@ -5,69 +5,118 @@ from __future__ import print_function
 import os
 import xml.etree.ElementTree as ET
 import numpy as np
+import re
 
 
 FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 DATA_PATH = os.path.join(FILE_PATH, "../data/")
-TEXTLINE_DATA_PATH = os.path.join(DATA_PATH, "original/")
+LABEL_DATA_PATH = os.path.join(DATA_PATH, "ascii/")
 STROKES_DATA_PATH = os.path.join(DATA_PATH, "lineStrokes/")
 
-# parse STROKES (.xml)
-text_line_data_all = []
-for path_1, _, _ in os.walk(STROKES_DATA_PATH):
-    for path_2, _, _ in os.walk(path_1):
-        for path_3, _, files in os.walk(path_2):
-            files = sorted(files)
-            for file_name in files:  # TextLine files
-                # split our .xml (eg: z01-000-01.xml -> z01-000-01)
-                text_line_id = file_name[:-3]
-                text_line_path = os.path.join(path_3, file_name)
-                e_tree = ET.parse(text_line_path).getroot()
-                x_list = []
-                y_list = []
-                time_list = []
-                first_time = 0.0
-                for atype in e_tree.findall('StrokeSet/Stroke/Point'):
-                    x_list.append(atype.get('x'))
-                    y_list.append(atype.get('y'))
-                    if len(time_list) == 0:
-                        first_time = float(atype.get('time'))
-                        time_list.append(0.0)
-                    else:
-                        time_list.append(float(atype.get('time')) - first_time)
-                    # print("x:", atype.get('x'), "y:", atype.get('y'), "time:", float(atype.get('time')) - first_time)
-                    # input()
-                x_list = np.asarray(x_list, dtype=np.float32)
-                y_list = np.asarray(y_list, dtype=np.float32)
-                time_list = np.asarray(time_list, dtype=np.float32)
-                # time 1st order
-                time_list_moved = np.array(time_list)
-                time_list_moved = time_list_moved[1:]
-                time_list_new = np.subtract(time_list_moved, time_list[:-1])
-                x_list_moved = np.array(x_list)
-                x_list_moved = x_list_moved[1:]
-                # x_list throw away the last element
-                x_list_new = np.subtract(x_list_moved, x_list[:-1])
-                x_list_new = np.divide(x_list_new, time_list_new)
-                # y 1st order
-                y_list_moved = np.array(y_list)
-                y_list_moved = y_list_moved[1:]
-                y_list_new = np.subtract(y_list_moved, y_list[:-1])
-                y_list_new = np.divide(y_list_new, time_list_new)
-                # stack x', y', time
-                text_line_data = np.stack(
-                    (x_list_new, y_list_new, time_list[:-1]), axis=1)
-                text_line_data_all.append(text_line_data)
-                # print(text_line_data)
-                # print(text_line_data.shape)
-                # print(text_line_path)
-                # print(np.array(text_line_data_all).shape)
-                # input()
-text_line_data_all = np.array(text_line_data_all)
-print(text_line_data_all.shape)
-np.save("test", text_line_data_all)
-# use TextLine id to parse text in original file (.xml)
 
+def find_textline_by_id(filename, de):
+    """
+    Inputs:
+        filename: string, textline prefix, eg: 'a01-020w-01'
+    Return:
+        label: string, label of one whole textline, eg: 'No secret talks # - Macleod.'
+    """
+    dir_name_L1 = filename[:3]  # eg: 'a01'
+    dir_name_L2 = filename[:7]  # eg: 'a01-020'
+    file_name = filename[:-3] + ".txt"  # eg: 'a01-020w.txt' or 'a01-020.txt'
+    line_id = int(filename[-2:])  # eg: 1
+    filepath = os.path.join(
+        LABEL_DATA_PATH, dir_name_L1, dir_name_L2, file_name)
+    line_counter = -2  # because line start after 2 new lines from "CSR:\n"
+    label = []
+    flag = False
+    for line in open(filepath, 'r'):
+        if line.startswith('CSR'):
+            flag = True
+        if flag:
+            line_counter += 1
+        if line_counter == line_id:
+            # line = re.sub('', '', line)
+            label = line
+            # TODO remove some common mark and check about empty cases
+    return label
+
+
+def main():
+    # parse STROKES (.xml)
+    text_line_data_all = []
+    label_text_line_all = []
+    for path_1, _, _ in os.walk(STROKES_DATA_PATH):
+        for path_2, _, _ in os.walk(path_1):
+            for path_3, _, files in os.walk(path_2):
+                files = sorted(files)
+                for file_name in files:  # TextLine files
+                    ############# label data #############
+                    # split our .xml (eg: a01-020w-01.xml -> a01-020w-01)
+                    text_line_id = file_name[:-4]
+                    # print (text_line_id)
+                    # print (file_name)
+                    label_text_line = find_textline_by_id(text_line_id,debug)
+                    label_text_line_all.append(label_text_line)
+                    ############# trajectory data #############
+                    text_line_path = os.path.join(path_3, file_name)
+                    e_tree = ET.parse(text_line_path).getroot()
+                    x_list = []
+                    y_list = []
+                    time_list = []
+                    first_time = 0.0
+                    for atype in e_tree.findall('StrokeSet/Stroke/Point'):
+                        x_list.append(atype.get('x'))
+                        y_list.append(atype.get('y'))
+                        if len(time_list) == 0:
+                            first_time = float(atype.get('time'))
+                            time_list.append(0.0)
+                        else:
+                            time_list.append(
+                                float(atype.get('time')) - first_time)
+                        # print("x:", atype.get('x'), "y:", atype.get('y'), "time:", float(atype.get('time')) - first_time)
+                        # input()
+                    x_list = np.asarray(x_list, dtype=np.float32)
+                    y_list = np.asarray(y_list, dtype=np.float32)
+                    time_list = np.asarray(time_list, dtype=np.float32)
+                    # time 1st order
+                    time_list_moved = np.array(time_list)
+                    time_list_moved = time_list_moved[1:]
+                    time_list_new = np.subtract(
+                        time_list_moved, time_list[:-1])
+                    for idx, v in enumerate(time_list_new):
+                        if v == 0:
+                            time_list_new[idx] = 0.001  # prevent divide by 0
+                    x_list_moved = np.array(x_list)
+                    x_list_moved = x_list_moved[1:]
+                    # x_list throw away the last element
+                    x_list_new = np.subtract(x_list_moved, x_list[:-1])
+                    x_list_new = np.divide(x_list_new, time_list_new)
+                    # y 1st order
+                    y_list_moved = np.array(y_list)
+                    y_list_moved = y_list_moved[1:]
+                    y_list_new = np.subtract(y_list_moved, y_list[:-1])
+                    y_list_new = np.divide(y_list_new, time_list_new)
+                    # stack x', y', time
+                    text_line_data = np.stack(
+                        (x_list_new, y_list_new, time_list[:-1]), axis=1)
+                    text_line_data_all.append(text_line_data)
+                    # print(text_line_data)
+                    # print(text_line_data.shape)
+                    # print(text_line_path)
+                    # print(np.array(text_line_data_all).shape)
+                    # input()
+    text_line_data_all = np.array(text_line_data_all)
+    label_text_line_all = np.array(label_text_line_all)
+    print(text_line_data_all.shape)
+    print(label_text_line_all.shape)
+    np.save("data", text_line_data_all)
+    np.save("label", label_text_line_all)
+    print("Successfully saved!")
+
+
+if __name__ == "__main__":
+    main()
 
 # # parse STROKES (.xml)
 # for path_1, dirs_1, _ in os.walk(STROKES_DATA_PATH):
---
+++ recognition/src/read.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+f = open('../data/letters.txt', 'r')
+
+l = []
+l.append(" ")
+for row in f:
+    print(l.append(row.strip('\n')))
+
+f.close()
+s = []
+k = np.load('label.npy')
+themax = 0
+for row in k:
+    print(row)
+    gg = row.strip('\n')
+    s.append(gg)
+    for aaa in gg:
+        if aaa in ['[',']']:
+            input()
+    if len(gg) > themax:
+        print(len(gg))
+        themax = len(gg)
+        print(gg.strip('.'))
+
+
+l += ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# for sentence in s:
+#     for char in sentence:
+#         if char not in l:
+#             print(sentence,"  ",char)
+
+dense = np.zeros((len(s), 64))
+print(l)
+for idl, sentence in enumerate(s):
+    sentence_iter = iter(sentence)
+    for idx, char in enumerate(sentence_iter):
+        if char == 'g' and sentence[idx:idx + 2] == 'ga':
+            dense[idl, idx] = l.index('ga')
+            next(sentence_iter)
+
+        elif char == 'k' and sentence[idx:idx + 2] == 'km':
+            dense[idl, idx] = l.index('km')
+            next(sentence_iter)
+
+        elif char == 'p' and sentence[idx:idx + 2] == 'pt':
+            dense[idl, idx] = l.index('pt')
+            next(sentence_iter)
+
+        elif char == 's' and sentence[idx:idx + 2] == 'sc':
+            dense[idl, idx] = l.index('sc')
+            next(sentence_iter)
+
+        elif char == 's' and sentence[idx:idx + 2] == 'sp':
+            dense[idl, idx] = l.index('sp')
+            next(sentence_iter)
+
+        elif char in l:
+            dense[idl, idx] = l.index(char)
+
+        else:
+            continue
+
+print(dense)
+
+np.save("dense.npy", dense)
+# print(row.strip('\n'))
+
+# import tensorflow as tf
+# a_t = tf.constant(dense)
+# idx = tf.where(tf.not_equal(a_t, 0))
+# sparse = tf.SparseTensor(idx, tf.gather_nd(a_t,idx), a_t.get_shape())
+
+
+"""
+a = np.reshape(np.arange(24), (3, 4, 2))
+with tf.Session() as sess:
+    a_t = tf.constant(a)
+    idx = tf.where(tf.not_equal(a_t, 0))
+    # Use tf.shape(a_t, out_type=tf.int64) instead of a_t.get_shape() if tensor shape is dynamic
+    sparse = tf.SparseTensor(idx, tf.gather_nd(a_t, idx), a_t.get_shape())
+    dense = tf.sparse_tensor_to_dense(sparse)
+    b = sess.run(dense)
+np.all(a == b)
+"""
